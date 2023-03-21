1fb9d94c8121b8d94f572a395de5be89472fc3c7
LorenzoTorchio <lorenzotorchios@gmail.com>
2023-03-21 15:02:19 -0300

    proyecto entrega 1

---
+++ paquete/Equipo.java
@@ -0,0 +1,7 @@
+package paquete;
+public class Equipo{
+    private String nombre;
+    public Equipo(String nombre){
+        this.nombre = nombre;
+    }
+}
---
+++ paquete/Ronda.java
@@ -0,0 +1,15 @@
+package paquete;
+
+public class Ronda{
+    private int num;
+    private Partido[] partidos;
+    public Ronda(int espacio){
+        this.partidos = new Partido[espacio];
+    }
+    public void setPartido(int i, Partido partido) {
+        partidos[i] = partido;
+    }
+    public Partido getPartido(int i){
+        return partidos[i];
+    }
+}
